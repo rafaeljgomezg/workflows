35e9e37b91795b52d9d5c04a807e627b936fcbee
rafaeljgomezg <rgomezshares@hotmail.com>
2016-08-28 18:24:52 -0400

    package.json update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-<<<<<<< HEAD
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
@@ -38,33 +37,4 @@ gulp.task('compass', function() {
     }))
     .on('error', gutil.log)
     .pipe(gulp.dest('builds/development/css'))
-=======
-	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee'); 
-	concat = require('gulp-concat');
-
-var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSources = [
-	'components/scripts/rclick.js',
-	'components/scripts/pixgrid.js',
-	'components/scripts/tagline.js',
-	'components/scripts/template.js'
-];
-
-gulp.task('log', function() {
-	gutil.log('workflows are awesome');
-});
-
-gulp.task('coffee', function() {
-	gulp.src(coffeeSources)
-	.pipe(coffee({bare:true})
-		.on('error',gutil.log))
-	.pipe(gulp.dest('components/scripts'));
-});
-
-gulp.task('js', function() {
-	gulp.src(jsSources)
-	.pipe(concat('script.js'))
-	.pipe(gulp.dest('builds/development/js'));
->>>>>>> d8844fd1ed1d6dd65939d4a29b238c0e52d24247
-});
+}); 
